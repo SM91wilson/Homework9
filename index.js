5f1ef74eb6d324d9bd9c5578756db8e59ec5e3dd
tango <sm91wilson@gmail.com>
2020-03-30 21:56:30 -0400

    added question prompts

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const inquirer = require("inquirer");
+
 const questions = [
+{
+    type: 'input',
+    message: 'Enter Username',
+    name: 'username'
+},
+{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'project-title'
+},
+{
+    type: 'input',
+    message: 'Give a brief description of your project',
+    name: 'project-description'
+}
 
 ];
 
+inquirer
+    .prompt(questions)
+    .then
 function writeToFile(fileName, data) {
 }
 
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,12 @@
+const axios = require('axios')
+var queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 const api = {
   getUser(username) {
-
+    axios
+    .get(queryUrl)
+    .then(function(userData){
+      console.log(userData.data)
+    })
   }
 };
 
